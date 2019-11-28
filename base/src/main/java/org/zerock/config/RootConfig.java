bfac385b93d2af3fa0a1861e94dc4f62af959a16
wonyong <zcx5674@gmail.com>
2019-11-28 22:05:03 +0900

    complete connection of mybatis and spring with hikariCP

--- base/src/main/java/org/zerock/config/RootConfig.java
+++ base/src/main/java/org/zerock/config/RootConfig.java
@@ -1,10 +1,43 @@
 package org.zerock.config;
 
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 @Configuration
 @ComponentScan(basePackages = {"org.zerock.sample"})
+@MapperScan(basePackages = {"org.zerock.mapper"})
 public class RootConfig {
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig hikariConfig = new HikariConfig();
+        //hikariConfig.setDriverClassName("oracle.jdbc.driver.OracleDriver"); 
+        //hikariConfig.setJdbcUrl("jdbc:oracle:thin:@localhost:49161:xe");
+        hikariConfig.setDriverClassName("net.sf.log4jdbc.sql.jdbcapi.DriverSpy"); // log4jdbc.log42j 설정후 
+        hikariConfig.setJdbcUrl("jdbc:log4jdbc:oracle:thin:@localhost:49161:xe"); // log4jdbc.log42j 설정후
+        
+        hikariConfig.setUsername("system");
+        hikariConfig.setPassword("oracle");
+
+        HikariDataSource dataSource = new HikariDataSource(hikariConfig);
+
+        return dataSource;
+    }
     
+    @Bean
+    public SqlSessionFactory sqlSessionFactory() throws Exception {
+        
+        SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
+        sqlSessionFactory.setDataSource(dataSource());
+        return (SqlSessionFactory) sqlSessionFactory.getObject();
+    }
 }
+
--- base/src/test/java/org/zerock/sample/SampleTests.java
+++ base/src/test/java/org/zerock/sample/SampleTests.java
@@ -7,12 +7,13 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.config.RootConfig;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@ContextConfiguration(classes= {RootConfig.class})
 @Log4j
 public class SampleTests {
     @Setter(onMethod_ = { @Autowired })
